0ef6d933ac8874f43ab7e4938945e9cf5e27b912
Suraj Kumar <sk96.uk@gmail.com>
2025-12-20 19:19:25 +0000

    feat: create Argon2PasswordEncoder bean

--- users-microservice/src/main/java/com/wannaverse/users/Main.java
+++ users-microservice/src/main/java/com/wannaverse/users/Main.java
@@ -2,10 +2,17 @@ package com.wannaverse.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
        SpringApplication.run(Main.class);
+    }
+
+    @Bean
+    public Argon2PasswordEncoder getArgon2PasswordEncoder() {
+        return new Argon2PasswordEncoder(16, 32, 1, 65536, 3);
     }
 }
