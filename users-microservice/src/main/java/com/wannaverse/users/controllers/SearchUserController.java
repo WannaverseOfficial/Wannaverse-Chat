3d5b865d9508a8d150a25a628cece1cb2d0de22d
Suraj Kumar <sk96.uk@gmail.com>
2025-12-21 21:46:50 +0000

    feat: create SearchUserController

---
+++ users-microservice/src/main/java/com/wannaverse/users/controllers/SearchUserController.java
@@ -0,0 +1,50 @@
+package com.wannaverse.users.controllers;
+
+import com.wannaverse.users.dto.SearchResults;
+import com.wannaverse.users.dto.UserDTO;
+import com.wannaverse.users.persistence.User;
+import com.wannaverse.users.services.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/user")
+public class SearchUserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public SearchUserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<?> searchUsers(
+            String query, Optional<Integer> page, Optional<Integer> size, Optional<Long> userId) {
+        if (userId.isPresent()) {
+            Optional<User> oUser = userService.getUserById(userId.get());
+
+            if (oUser.isEmpty()) {
+                return ResponseEntity.ok(new SearchResults<>(List.of(), 0, 0));
+            }
+
+            User user = oUser.get();
+            UserDTO userDTO =
+                    new UserDTO(
+                            user.getId(),
+                            user.getFirstName(),
+                            user.getFirstName(),
+                            user.getDisplayName());
+
+            return ResponseEntity.ok(userDTO);
+        }
+        return ResponseEntity.ok(userService.search(query, page.orElse(0), size.orElse(10)));
+    }
+}
