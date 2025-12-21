899b145fc019e6f4611ef4303165fffea4c20db7
Suraj Kumar <sk96.uk@gmail.com>
2025-12-21 21:46:31 +0000

    feat: add search to repo and service

--- users-microservice/src/main/java/com/wannaverse/users/services/UserService.java
+++ users-microservice/src/main/java/com/wannaverse/users/services/UserService.java
@@ -1,10 +1,17 @@
 package com.wannaverse.users.services;
 
+import com.wannaverse.users.dto.SearchResults;
+import com.wannaverse.users.dto.UserDTO;
 import com.wannaverse.users.persistence.User;
 import com.wannaverse.users.persistence.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -21,5 +28,29 @@ public class UserService {
 
     public void save(User user) {
         userRepository.save(user);
+    }
+
+    public Optional<User> getUserById(long id) {
+        return userRepository.findUserById(id);
+    }
+
+    public SearchResults<UserDTO> search(String query, int page, int size) {
+        Page<User> results =
+                userRepository
+                        .findByEmailAddressContainingIgnoreCaseOrFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCaseOrDisplayNameContainingIgnoreCase(
+                                query, query, query, query, PageRequest.of(page, size));
+
+        List<UserDTO> transformed =
+                results.getContent().stream()
+                        .map(
+                                user ->
+                                        new UserDTO(
+                                                user.getId(),
+                                                user.getFirstName(),
+                                                user.getLastName(),
+                                                user.getDisplayName()))
+                        .toList();
+
+        return new SearchResults<>(transformed, results.getNumber(), results.getTotalPages());
     }
 }
