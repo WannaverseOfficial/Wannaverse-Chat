eff0ca272c444eb17a994eb285266b15860e372b
Suraj Kumar <sk96.uk@gmail.com>
2025-12-21 22:06:38 +0000

    chore: move discovery into com.wannaverse.discovery

--- discovery-service/src/main/java/com/wannaverse/discovery/Main.java
+++ discovery-service/src/main/java/com/wannaverse/discovery/Main.java
@@ -1,4 +1,4 @@
-package org.christolis;
+package com.wannaverse.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
