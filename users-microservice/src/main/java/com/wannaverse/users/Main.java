99f5b8f92b170f0dff4eea431a87d60db71f282e
Suraj Kumar <sk96.uk@gmail.com>
2025-12-22 12:20:30 +0000

    create api-gateway module (#22)
    * chore: add eureka client and gateway to libs.versions.toml
    
    * feat: create api-gateway module
    
    * feat: register users-microservice with eureka
    
    * chore: move discovery into com.wannaverse.discovery
    
    * chore: yml -> yaml

--- users-microservice/src/main/java/com/wannaverse/users/Main.java
+++ users-microservice/src/main/java/com/wannaverse/users/Main.java
@@ -2,10 +2,12 @@ package com.wannaverse.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
