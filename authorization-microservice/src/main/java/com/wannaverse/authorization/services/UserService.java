50c0d96fecbf06ad55c6ffc60b452e73f821238f
Suraj Kumar <sk96.uk@gmail.com>
2025-12-24 14:12:23 +0000

    use kafka to send "new user event" and create user in users-microservice (#38)
    * chore: add kafka dependency
    
    * chore: add kafka configuration to application.yaml
    
    * feat: send userId on `new-user-registration` topic
    
    * feat: consume event from kafka and create user

--- authorization-microservice/src/main/java/com/wannaverse/authorization/services/UserService.java
+++ authorization-microservice/src/main/java/com/wannaverse/authorization/services/UserService.java
@@ -3,7 +3,10 @@ package com.wannaverse.authorization.services;
 import com.wannaverse.authorization.persistence.User;
 import com.wannaverse.authorization.persistence.UserRepository;
 
+import jakarta.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,19 +16,27 @@ import java.util.Optional;
 public class UserService {
     private final UserRepository userRepository;
     private final Argon2PasswordEncoder passwordEncoder;
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
     @Autowired
-    public UserService(UserRepository userRepository, Argon2PasswordEncoder passwordEncoder) {
+    public UserService(
+            UserRepository userRepository,
+            Argon2PasswordEncoder passwordEncoder,
+            KafkaTemplate<String, String> kafkaTemplate) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.kafkaTemplate = kafkaTemplate;
     }
 
     public boolean isEmailAddressInUse(String email) {
         return userRepository.findUserByEmailAddressIgnoreCase(email).isPresent();
     }
 
+    @Transactional
     public void save(User user) {
         userRepository.save(user);
+
+        kafkaTemplate.send("new-user-registration", user.getId());
     }
 
     public Optional<User> getUserByEmail(String emailAddress) {
