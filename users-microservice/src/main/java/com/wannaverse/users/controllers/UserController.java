b3c1581d86b8e6703236c866dff5b97449f57a3c
Chris Sdogkos <work@chris-sdogkos.com>
2025-12-27 03:42:23 +0300

    wip(users-microservice): introduce /avatar endpoint
    We need a way for users to upload their profile picture and fetch it as
    well. Introduce the relevant endpoints.
    
    Currently PNG, JPEG and GIF are supported for uploading, and only PNG
    for retrieving, but that should change soon.
    
    Signed-off-by: Chris Sdogkos <work@chris-sdogkos.com>

--- users-microservice/src/main/java/com/wannaverse/users/controllers/UserController.java
+++ users-microservice/src/main/java/com/wannaverse/users/controllers/UserController.java
@@ -3,18 +3,36 @@ package com.wannaverse.users.controllers;
 import com.wannaverse.users.persistence.User;
 import com.wannaverse.users.services.UserService;
 
+import jakarta.annotation.Nullable;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.Principal;
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    private static final List<String> ACCEPTED_TYPES =
+            List.of(
+                    MediaType.IMAGE_PNG_VALUE,
+                    MediaType.IMAGE_JPEG_VALUE,
+                    MediaType.IMAGE_GIF_VALUE);
+    private static final String AVATAR_DIRECTORY_PATH = "avatars";
     private final UserService userService;
 
     @Autowired
@@ -33,5 +51,50 @@ public class UserController {
                         () -> userService.createUser(userId, user));
 
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/avatar")
+    public ResponseEntity<?> getAvatar(String userId) throws IOException {
+        if (userId.isBlank()) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        Path avatarPath = Path.of(AVATAR_DIRECTORY_PATH + "/" + userId);
+
+        if (!Files.exists(avatarPath)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_PNG);
+
+        return new ResponseEntity<>(
+                Files.readAllBytes(avatarPath), headers, HttpStatusCode.valueOf(200));
+    }
+
+    @PostMapping("/avatar")
+    public ResponseEntity<String> uploadAvatar(
+            @RequestBody @Nullable MultipartFile multipartFile, Principal principal)
+            throws IOException {
+
+        if (multipartFile == null) {
+            return ResponseEntity.badRequest().body("No image.");
+        }
+
+        String contentType = multipartFile.getContentType();
+
+        if (!ACCEPTED_TYPES.contains(contentType)) {
+            return ResponseEntity.badRequest().body("Shit file type");
+        }
+
+        Path uploadPath = Paths.get(AVATAR_DIRECTORY_PATH);
+
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+
+        multipartFile.transferTo(uploadPath.resolve(principal.getName()));
+
+        return ResponseEntity.ok("Profile picture has been uploaded.");
     }
 }
